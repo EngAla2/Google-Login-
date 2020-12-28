f0374be48b2d732432af4438b9bf10efb5504c78
EngAla2 <Eng4Ala2@gmail.com>
2020-12-28 19:14:22 +0200

    update

--- app/src/main/java/com/example/myloginapp/LinkedInEmailModel.kt
+++ app/src/main/java/com/example/myloginapp/LinkedInEmailModel.kt
@@ -1,2 +1,23 @@
 package com.example.myloginapp
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class LinkedInEmailModel(
+    val elements: List<ElementEmail>
+)
+
+@Serializable
+data class ElementEmail(
+    @SerialName("handle~")
+    val elementHandle: Handle,
+
+    val handle: String
+)
+
+@Serializable
+data class Handle(
+    val emailAddress: String
+)
--- app/src/main/java/com/example/myloginapp/MainActivity.kt
+++ app/src/main/java/com/example/myloginapp/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.example.myloginapp
 
+import android.annotation.SuppressLint
+import android.annotation.TargetApi
+import android.app.Dialog
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.facebook.CallbackManager
@@ -19,94 +27,207 @@ import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
+import org.json.JSONObject
+import org.json.JSONTokener
+import java.io.OutputStreamWriter
+import java.net.URL
+import java.util.concurrent.TimeUnit
+import javax.net.ssl.HttpsURLConnection
 
 
 const val GOOGLE_RC_SIGN_IN = 123
 
 class MainActivity : AppCompatActivity() {
+    lateinit var linkedinAuthURLFull: String
+    lateinit var linkedIndialog: Dialog
+    lateinit var linkedinCode: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val state = "linkedin" + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis())
 
-        var callbackManager = CallbackManager.Factory.create();
-        try {
-            facebook.registerCallback(callbackManager,
-                object : FacebookCallback<LoginResult?> {
-                    override fun onSuccess(loginResult: LoginResult?) {
-                        println("pass")
-                    }
+        linkedinAuthURLFull =
+            LinkedInConstants.AUTHURL + "?response_type=code&client_id=" + LinkedInConstants.CLIENT_ID + "&scope=" + LinkedInConstants.SCOPE + "&state=" + state + "&redirect_uri=" + LinkedInConstants.REDIRECT_URI
 
-                    override fun onCancel() {
-                        println("cancel")
-                    }
-
-                    override fun onError(exception: FacebookException) {
-                        println("error")
-                    }
-                })
-        }catch (e:Exception){
-            println("--------------------------$e-----------------------")
-        }
-
-
-
-            logout.setOnClickListener { signOut() }
-            facebook.setOnClickListener { }
-
-
-        }
-
-    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when(requestCode){
-
+        `in`.setOnClickListener {
+            setupLinkedinWebviewDialog(linkedinAuthURLFull)
         }
 
     }
 
-    private fun handleSignInResultFacebook(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val account = completedTask.getResult(
-                ApiException::class.java
-            )
-            // Signed in successfully
+    // Show LinkedIn login page in a dialog
+    @SuppressLint("SetJavaScriptEnabled")
+    fun setupLinkedinWebviewDialog(url: String) {
+        linkedIndialog = Dialog(this)
+        val webView = WebView(this)
+        webView.isVerticalScrollBarEnabled = false
+        webView.isHorizontalScrollBarEnabled = false
+        webView.webViewClient = LinkedInWebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl(url)
+        linkedIndialog.setContentView(webView)
+        linkedIndialog.show()
+    }
 
-            val googleId = account?.id ?: ""
-            id_textview.text = googleId
+    // A client to know about WebView navigations
+    // For API 21 and above
+    @Suppress("OverridingDeprecatedMember")
+    inner class LinkedInWebViewClient : WebViewClient() {
 
-            val googleFirstName = account?.givenName ?: ""
-            first_name_textview.text = googleFirstName
+        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            if (request?.url.toString().startsWith(LinkedInConstants.REDIRECT_URI)) {
+                handleUrl(request?.url.toString())
 
-            val googleLastName = account?.familyName ?: ""
-            last_name_textview.text = googleLastName
+                // Close the dialog after getting the authorization code
+                if (request?.url.toString().contains("?code=")) {
+                    linkedIndialog.dismiss()
+                }
+                return true
+            }
+            return false
+        }
 
-            val googleEmail = account?.email ?: ""
-            email_textview.text = googleEmail
+        // For API 19 and below
+        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+            if (url.startsWith(LinkedInConstants.REDIRECT_URI)) {
+                handleUrl(url)
 
-            val googleProfilePicURL = account?.photoUrl.toString()
-            profile_picture_url_textview.text = googleProfilePicURL
+                // Close the dialog after getting the authorization code
+                if (url.contains("?code=")) {
+                    linkedIndialog.dismiss()
+                }
+                return true
+            }
+            return false
+        }
 
-            val googleIdToken = account?.idToken ?: ""
-            access_token_textview.text = googleIdToken
+        // Check webview url for access token code or error
+        private fun handleUrl(url: String) {
+            val uri = Uri.parse(url)
 
-            mainContainer.visibility = View.GONE
-            userInfo.visibility = View.VISIBLE
-        } catch (e: ApiException) {
-            // Sign in was unsuccessful
-            Log.e(
-                "failed code=", e.statusCode.toString()
-            )
-            // The ApiException status code indicates the detailed failure reason.
-            println("------------------------------------------------------------------")
-            println(e.toString())
-            println("------------------------------------------------------------------")
-
+            if (url.contains("code")) {
+                linkedinCode = uri.getQueryParameter("code") ?: ""
+                linkedInRequestForAccessToken()
+            } else if (url.contains("error")) {
+                val error = uri.getQueryParameter("error") ?: ""
+                Log.e("Error: ", error)
+            }
         }
     }
-    private fun signOut() {
+    fun linkedInRequestForAccessToken() {
+        GlobalScope.launch(Dispatchers.Default) {
+            val grantType = "authorization_code"
+            val postParams =
+                "grant_type=" + grantType + "&code=" + linkedinCode + "&redirect_uri=" + LinkedInConstants.REDIRECT_URI + "&client_id=" + LinkedInConstants.CLIENT_ID + "&client_secret=" + LinkedInConstants.CLIENT_SECRET
+            val url = URL(LinkedInConstants.TOKENURL)
+            val httpsURLConnection = withContext(Dispatchers.IO) {url.openConnection() as HttpsURLConnection }
+            httpsURLConnection.requestMethod = "POST"
+            httpsURLConnection.setRequestProperty(
+                "Content-Type",
+                "application/x-www-form-urlencoded"
+            )
+            httpsURLConnection.doInput = true
+            httpsURLConnection.doOutput = true
+            val outputStreamWriter = OutputStreamWriter(httpsURLConnection.outputStream)
+            withContext(Dispatchers.IO) {
+                outputStreamWriter.write(postParams)
+                outputStreamWriter.flush()
+            }
+            val response = httpsURLConnection.inputStream.bufferedReader()
+                .use { it.readText() }  // defaults to UTF-8
+            val jsonObject = JSONTokener(response).nextValue() as JSONObject
 
+            val accessToken = jsonObject.getString("access_token") //The access token
+            Log.d("accessToken is: ", accessToken)
+
+            val expiresIn = jsonObject.getInt("expires_in") //When the access token expires
+            Log.d("expires in: ", expiresIn.toString())
+
+
+            withContext(Dispatchers.Main) {
+                // Get user's id, first name, last name, profile pic url
+                fetchlinkedInUserProfile(accessToken)
+            }
+        }
+    }
+    fun fetchlinkedInUserProfile(token: String) {
+        GlobalScope.launch(Dispatchers.Default) {
+            val tokenURLFull =
+                "https://api.linkedin.com/v2/me?projection=(id,firstName,lastName,profilePicture(displayImage~:playableStreams))&oauth2_access_token=$token"
+            val url = URL(tokenURLFull)
+            val httpsURLConnection = withContext(Dispatchers.IO) {url.openConnection() as HttpsURLConnection}
+            httpsURLConnection.requestMethod = "GET"
+            httpsURLConnection.doInput = true
+            httpsURLConnection.doOutput = false
+            val response = httpsURLConnection.inputStream.bufferedReader()
+                .use { it.readText() }  // defaults to UTF-8
+            val linkedInProfileModel =
+                Json.nonstrict.parse(LinkedInProfileModel.serializer(), response)
+            withContext(Dispatchers.Main) {
+                Log.d("LinkedIn Access Token: ", token)
+
+                // LinkedIn Id
+                val linkedinId = linkedInProfileModel.id
+                Log.d("LinkedIn Id: ", linkedinId)
+
+                // LinkedIn First Name
+                val linkedinFirstName = linkedInProfileModel.firstName.localized.enUS
+                Log.d("LinkedIn First Name: ", linkedinFirstName)
+
+                // LinkedIn Last Name
+                val linkedinLastName = linkedInProfileModel.lastName.localized.enUS
+                Log.d("LinkedIn Last Name: ", linkedinLastName)
+
+                // LinkedIn Profile Picture URL
+                /*
+                     Change row of the 'elements' array to get diffrent size of the profile pic
+                     elements[0] = 100x100
+                     elements[1] = 200x200
+                     elements[2] = 400x400
+                     elements[3] = 800x800
+                */
+
+                val linkedinProfilePic =
+                    linkedInProfileModel.profilePicture.displayImage.elements.get(2)
+                        .identifiers.get(0).identifier
+                Log.d("LinkedIn Profile URL: ", linkedinProfilePic)
+
+                // Get user's email address
+                fetchLinkedInEmailAddress(token)
+            }
+        }
+    }
+    fun fetchLinkedInEmailAddress(token: String) {
+        val tokenURLFull =
+            "https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))&oauth2_access_token=$token"
+
+        GlobalScope.launch(Dispatchers.Default) {
+            val url = URL(tokenURLFull)
+            val httpsURLConnection = withContext(Dispatchers.IO) {url.openConnection() as HttpsURLConnection}
+            httpsURLConnection.requestMethod = "GET"
+            httpsURLConnection.doInput = true
+            httpsURLConnection.doOutput = false
+            val response = httpsURLConnection.inputStream.bufferedReader()
+                .use { it.readText() }  // defaults to UTF-8
+            val linkedInProfileModel =
+                Json.nonstrict.parse(LinkedInEmailModel.serializer(), response)
+            withContext(Dispatchers.Main) {
+                // LinkedIn Email
+                val linkedinEmail = linkedInProfileModel.elements.get(0).elementHandle.emailAddress
+                Log.d("LinkedIn Email: ", linkedinEmail)
+            }
+        }
     }
 }
